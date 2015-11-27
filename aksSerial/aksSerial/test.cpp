e2d8a9881d4108910e40703f99d7a303752d56f9
Divyanshu Maithani <div.blackcat@gmail.com>
2015-11-27 10:07:29 +0530

    Update test.cpp with aksLnP() for #1

--- aksSerial/aksSerial/test.cpp
+++ aksSerial/aksSerial/test.cpp
@@ -18,19 +18,36 @@ void testIsPower() {
 		std::cout << " is NOT perfect power" << std::endl;
 }
 
+void testAksLnP() {
+  mpz_t number;
+	char ip[100];
+  printf("Input number to check for AKS LnP\n");
+	cin >> ip;
+	mpz_init_set_str(number, ip, 10);
+	gmp_printf("%Zd \n", number);
+	if(aksLnP(number))
+		std::cout << " is prime" << std::endl;
+	else
+		std::cout << " is composite" << std::endl;
+}
+
 int main()
 {
   printf("AKS Test class\n\
           1. Test isPower()\
           2. Test getOrder()\
           3. Test getMinR()\
-          4. Exit\n\
+          4. Test aksLnP()\
+          5. Exit\n\
           Input your choice\n");
   int choice;
   scanf("%d", &choice);
   switch (choice) {
     case 1:
       testIsPower();
+      break;
+    case 4:
+      testAksLnP();
       break;
     default:
       break;
